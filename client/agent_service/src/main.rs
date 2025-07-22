ef4a8f78694c9f5ab6e0db143ce89d2bd1a8bc3d
赵光建 <zhaoguangjian@cyberkl.com>
2025-07-22 18:25:04 +0800

    add rustls

--- client/agent_service/src/main.rs
+++ client/agent_service/src/main.rs
@@ -1,9 +1,12 @@
 use agent_service::{config::AGENT_SERVICE_TOML, proto::HeartbeatReq};
+use rustls::crypto::{CryptoProvider, ring};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     log4rs::init_file("./config/log4rs.yml", Default::default())?;
     log::info!("agent service starting");
+    CryptoProvider::install_default(ring::default_provider())
+        .expect("failed to install CryptoProvider");
 
     let mut client = agent_service::build_client(&AGENT_SERVICE_TOML.server.addr).await?;
     let req = HeartbeatReq {
