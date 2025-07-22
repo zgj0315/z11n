77c067af3f5ef11dda5e3067f247a2ae88f2174b
赵光建 <zhaoguangjian@cyberkl.com>
2025-07-22 17:24:39 +0800

    agent service not work

---
+++ client/agent_service/src/lib.rs
@@ -0,0 +1,18 @@
+use crate::proto::z11n_service_client::Z11nServiceClient;
+use std::fs;
+use tonic::transport::{Certificate, Channel, ClientTlsConfig};
+
+pub mod proto {
+    tonic::include_proto!("z11n");
+}
+pub mod config;
+
+pub async fn build_client(url: &'static str) -> anyhow::Result<Z11nServiceClient<Channel>> {
+    let pem = fs::read("./config/z11n-ca.crt")?;
+    let ca = Certificate::from_pem(pem);
+    let tls = ClientTlsConfig::new()
+        .ca_certificate(ca)
+        .domain_name("z11n.com");
+    let channel = Channel::from_static(url).tls_config(tls)?.connect().await?;
+    Ok(Z11nServiceClient::new(channel))
+}
--- client/agent_service/src/main.rs
+++ client/agent_service/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use agent_service::{config::AGENT_SERVICE_TOML, proto::HeartbeatReq};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    log4rs::init_file("./config/log4rs.yml", Default::default())?;
+    log::info!("agent service starting");
+
+    let mut client = agent_service::build_client(&AGENT_SERVICE_TOML.server.addr).await?;
+    let req = HeartbeatReq {
+        agent_id: "".to_string(),
+        agent_type: "".to_string(),
+    };
+    let rsp = client.heartbeat(req).await?;
+    log::info!("{:?}", rsp);
+    Ok(())
 }
